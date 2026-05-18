030b7db533cb367969f2be817fc3d48cc297c3ca
jay-johnson <jay.p.h.johnson@gmail.com>
2026-05-18 03:36:55 +0000

    doc fix the warning from the error:  NameError: name 'Any' is not defined. Did you mean: 'any'?

--- oats/session/models.py
+++ oats/session/models.py
@@ -4,7 +4,7 @@
 Coder Session Models
 """
 
-from typing import Dict, Optional
+from typing import Dict, Optional, Any
 from pydantic import BaseModel
 from pydantic.config import ConfigDict
 from oats.tool.registry import Tool
